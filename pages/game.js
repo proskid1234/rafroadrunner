41c66e865a80f7cda1fccb16ec147b3dc23e4c47
proskid1234 <110002694+proskid1234@users.noreply.github.com>
2025-05-15 09:30:16 -0400

    Create game.js

---
+++ pages/game.js
@@ -0,0 +1,8 @@
+export default function Game() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🛣️ RaphRoadRunner</h1>
+      <p>Game loading... (this is a placeholder)</p>
+    </div>
+  );
+}
